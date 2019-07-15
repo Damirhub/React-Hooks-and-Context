badf75a562823316e5a96e3d4a3c410da001f299
Damirhub <damir.pjetlovic@gmail.com>
2019-07-15 10:35:54 +0200

    context example

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Routes from './routes';
+import Context from './utils/context';
+
+
+
+
+const App = () =>  {
+  
+
+  const [stateGlobal, setStateGlobal] = useState(0)
+  
+  const incrementGlobalState = () => {
+    setStateGlobal (stateGlobal +1)
+  }
+
+  const decrementGlobalState = () => {
+    setStateGlobal (stateGlobal +1)
+  }
+
+    return(
+      <div>
+      React
+      <Context.Provider
+        value = {{
+          valueGlobalState: stateGlobal,
+          addGlobalValue: () => incrementGlobalState(),
+          decGlobalValue: () => decrementGlobalState()
+        }}>
+      <Routes />
+      </Context.Provider>
+      
+      </div>
+    )
+}
+
+
+export default App;
---
+++ src/hooks/hooks_container.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect, useReducer, useContext } from 'react';
+
+import * as Reducer from '../store/hooks_state/hooks_reducer'
+import * as ACTIONS from '../store/actions/actions'
+import Krofna from '../utils/context';
+
+const HooksContainer1 = () => {
+    
+    const context = useContext(Krofna)
+    console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", context)
+
+
+
+    const [stateValue, setValue] = useState(0)
+    const [someValue, setSomeValue] = useState(null)
+
+    const [state, dispatch] = useReducer(Reducer.HooksReducer, Reducer.initialState)
+
+    useEffect( () => {
+        setTimeout(()=> setSomeValue("useEffect WorkeD!"), 3000)
+    }, [stateValue])
+
+    const incrementValue = () => {
+        setValue(stateValue +1)
+    }
+    const decrementValue = () => {
+        setValue(stateValue - 1)
+    }
+
+    const changeUseEffectValue = () => {
+        setSomeValue('Some string to show changed value')
+    }
+
+    const handleDispatchTrue = () => {
+        // dispatch(ACTIONS.SUCCESS)
+         // dispatch(type: "SUCCESS")
+
+        dispatch(ACTIONS.success())
+    }
+
+    const handleDispatchFalse = () => {
+        dispatch(ACTIONS.failure())
+    }
+    return (
+        <div>
+            React hooks
+            <button onClick = { () => incrementValue()}>Inc Local State </button>
+            <button onClick = { () => decrementValue()}>Dec Local State </button>
+            <button onClick = { () => changeUseEffectValue()}>change Use Effect Value </button>
+
+            <button onClick = { () => handleDispatchTrue()}>Dispatch Truee </button>
+            <button onClick = { () => handleDispatchFalse()}>Dispatch False </button>
+
+            <button onClick = { () => context.addGlobalValue()}>Add Global </button>
+            <button onClick = { () => context.decGlobalValue()}>Dec Global </button>
+
+            <div>
+                <br/>
+                    {
+                    someValue ? <p>{someValue}</p> : <p>No value</p> 
+                    }
+                <br/>
+                {state.stateprop1
+                ? <p> state prop1 is true</p> : <p> state prop1 is False</p>}
+                <br/>
+                <p>Local State: {stateValue} </p>
+                <br/>
+                <hr/>
+                <p>Global State: {context.valueGlobalState}</p>
+
+            </div>
+
+            <h2>{stateValue}</h2>
+        </div>
+    );
+};
+
+export default HooksContainer1;
